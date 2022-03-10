b9ca42a12a430f6807785196ae26b4a58fc94f27
vandoan882000 <vandoan882000@gmail.com>
2022-03-10 11:56:26 +0700

    fix js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,6 +4,22 @@ const iconTime = document.getElementById("icon-time");
 const menu = document.getElementById("nav");
 const menuMobile = document.getElementById("nav-mobile");
 menuMobile.style.display = "none";
+(function initMenu() {
+  if(window.innerWidth < 1000)
+  {
+    iconBar.style.display = "block";
+    iconTime.style.display = "none";
+    menuMobile.style.display = "none";
+    menu.style.display = "none";
+  }
+  else
+  {
+    iconBar.style.display = "none";
+    iconTime.style.display = "none";
+    menuMobile.style.display = "none";
+    menu.style.display = "flex";
+  }
+})();
 function resizeScreen(){
   if(window.innerWidth < 1000)
   {
@@ -19,7 +35,6 @@ function resizeScreen(){
     menuMobile.style.display = "none";
     menu.style.display = "flex";
   }
-
 }
 iconBar.addEventListener("click", function click(){
   iconBar.style.display = "none";
@@ -31,3 +46,14 @@ iconTime.addEventListener("click", function click(){
   iconTime.style.display = "none";
   menuMobile.style.display = "none";
 });
+const menuList = document.querySelectorAll("menu-mobile__list");
+function clickDropDown(li) {
+  if(li.className == "menu-mobile__list"){
+    li.className =li.className + "--active";
+    console.log(li.className + "");
+  }
+  else
+  {
+    li.className = "menu-mobile__list";
+  }
+};
