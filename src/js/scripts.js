12ffeb996fa5c7f32947e06db758245be1c83de2
vandoan882000 <vandoan882000@gmail.com>
2022-03-10 20:06:00 +0700

    add jsom

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,20 +3,19 @@ const iconBar = document.getElementById("icon-bar");
 const iconTime = document.getElementById("icon-time");
 const menu = document.getElementById("nav");
 const menuMobile = document.getElementById("nav-mobile");
-menuMobile.style.display = "none";
 (function initMenu() {
   if(window.innerWidth < 1000)
   {
     iconBar.style.display = "block";
     iconTime.style.display = "none";
-    menuMobile.style.display = "none";
+    menuMobile.className = "nav-mobile";
     menu.style.display = "none";
   }
   else
   {
     iconBar.style.display = "none";
     iconTime.style.display = "none";
-    menuMobile.style.display = "none";
+    menuMobile.className = "nav-mobile";
     menu.style.display = "flex";
   }
 })();
@@ -25,31 +24,38 @@ function resizeScreen(){
   {
     iconBar.style.display = "block";
     iconTime.style.display = "none";
-    menuMobile.style.display = "none";
+    menuMobile.className = "nav-mobile";
     menu.style.display = "none";
   }
   else
   {
     iconBar.style.display = "none";
     iconTime.style.display = "none";
-    menuMobile.style.display = "none";
+    menuMobile.className = "nav-mobile";
     menu.style.display = "flex";
   }
+}
+function handleClickCloseMenu() {
+  iconBar.style.display = "block";
+  iconTime.style.display = "none";
+  menuMobile.className = "nav-mobile";
 }
 iconBar.addEventListener("click", function click(){
   iconBar.style.display = "none";
   iconTime.style.display = "block";
-  menuMobile.style.display = "block";
+  menuMobile.className = "nav-mobile--active";
 });
 iconTime.addEventListener("click", function click(){
   iconBar.style.display = "block";
   iconTime.style.display = "none";
-  menuMobile.style.display = "none";
+  menuMobile.className = "nav-mobile";
 });
-const menuList = document.querySelectorAll("menu-mobile__list");
-function clickDropDown(li) {
+const menuList = document.querySelectorAll(".menu-mobile__list");
+
+
+function handleClickDropDown(li) {
   if(li.className == "menu-mobile__list"){
-    li.className =li.className + "--active";
+    li.className = li.className + "--active";
     console.log(li.className + "");
   }
   else
